8f7bf4495c64b8b193dfdb304981ad067aa4ca24
yoi-evernacht <93626422+yoi-evernacht@users.noreply.github.com>
2024-07-22 19:48:40 +0900

    最低限の引数を設定する

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::{Arg, ArgAction, Command, ValueHint};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("Ls_Media")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("ls but for madia files")
+        // path
+        .arg(
+            Arg::new("path")
+                .action(ArgAction::Set)
+                .value_name("PATH")
+                .value_hint(ValueHint::DirPath)
+                .index(1)
+        )
+        // long
+        .arg(
+            Arg::new("long")
+                .action(ArgAction::SetTrue)
+                .long("long")
+                .short('l')
+                .help("Display extended info")
+        )
+        .get_matches();
 }
